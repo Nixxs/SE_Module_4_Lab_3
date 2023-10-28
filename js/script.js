f1eadcab81ac6b97b4c67659b4c0dba1f8a87ebb
Nicholas Chai <nicholas.chai@outlook.com>
2023-10-28 14:33:48 +0800

    completed reset logic

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function reset(){
+    document.getElementById("input-num-one").value = "";
+    document.getElementById("input-num-two").value = "";
+    document.getElementById("result-output").value = "";
+}
