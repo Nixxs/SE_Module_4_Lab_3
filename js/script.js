ccf12cfb4990b9488aa4840b33788513f2505065
Nicholas Chai <nicholas.chai@outlook.com>
2023-10-28 14:31:01 +0800

    completed calcualtor logic

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function calculate() {
+    let result;
+    let num_1 = Number(document.getElementById("input-num-one").value);
+    let num_2 = Number(document.getElementById("input-num-two").value);
+    let operator = document.getElementById("operator-input").value;
+
+    switch (operator) {
+        case "add":
+            result = num_1 + num_2;
+            break;
+        case "subtract":
+            result = num_1 - num_2;
+            break;
+        case "multiply":
+            result = num_1 * num_2;
+            break;
+        case "divide":
+            result = num_1 / num_2;
+            break;
+        default:
+            result = "Invalid number"
+    }
+
+    document.getElementById("result-output").value = result;
+}
